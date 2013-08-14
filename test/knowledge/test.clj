def5f28c6d27818f9cbfdf6ff96d855191c9cb3d
Mingli Yuan <mingli.yuan@gmail.com>
2013-08-14 11:04:56 +0800

    fix tests accordingly

--- test/knowledge/test.clj
+++ test/knowledge/test.clj
@@ -2,11 +2,11 @@
   (:refer-clojure :exclude [==])
   (:require [clojure.test :as test])
   (:use clojure.core.logic
-        meta.properties
+        meta.meta
         entities.albert-einstein))
 
 (test/deftest test-place-of-death 
   (test/is (= (first (first (run* [q]
     (fresh [x]
-      (place-of-death "Albert Einstein" x)
+      (claim "Albert Einstein" "place-of-death" x)
         (== q [x])))))) "Princeton"))
